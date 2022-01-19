8813563c93ca5816308f68b6d6976816fd443099
Ruhika <ruhika406@gmail.com>
2022-01-19 17:00:33 +0530

    updated add.py

--- add.py
+++ add.py
@@ -1,7 +1,13 @@
 
-// a Small python file to print 2 numbers in python3 
-a=10
-b=30
-print(a+b)
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 
 
